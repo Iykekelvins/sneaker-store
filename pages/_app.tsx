b20de423b82283ef36b6ad00f2f79d41930ed47f
Kelvin Ochubili <kelvinochubili@gmail.com>
2022-06-13 18:01:00 +0100

    fixed slug routing issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,22 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/Layout/Layout';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [ready, setReady] = useState(false)
+  const router = useRouter()
+  
+  useEffect(() => {
+    if (router.isReady) {
+      setReady(true)
+      }
+}, [router.isReady])
+
   return <Provider store={store}>
           <Layout >
-              <Component {...pageProps} />
+          {ready &&  <Component {...pageProps} />}
         </Layout>
         </Provider>
 }
